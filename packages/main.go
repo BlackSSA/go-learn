38378d61804c8952bdc028daa108556d691fc268
Sergey Slobodyanyuk <sslobodyanyuk@ocs.ru>
2023-09-10 19:41:48 +0300

    t

--- packages/main.go
+++ packages/main.go
@@ -2,13 +2,22 @@ package main
 
 import (
 	"fmt"
+	. "packages/fmt"
 	"packages/store"
+	"packages/store/cart"
+	//"github.com/fatih/color"
 )
 
 func main() {
 	product := store.NewProduct("Kayak", "Watersports", 279)
 
-	fmt.Println("Name:", product.Name)
-	fmt.Println("Category:", product.Category)
-	fmt.Println("Price:", product.Price())
+	cart := cart.Cart{
+		CustomerName: "Alice",
+		Products:     []store.Product{*product},
+	}
+
+	fmt.Println("Name: ", cart.CustomerName)
+	fmt.Println("Total: ", ToCurrency(cart.GetTotal()))
+	// color.Red("Name: " + cart.CustomerName)
+	// color.Cyan("Total: " + ToCurrency(cart.GetTotal()))
 }
