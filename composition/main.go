d41bef7cbc213b6a3730ccbd51bd398703d816f7
Sergey Slobodyanyuk <sslobodyanyuk@ocs.ru>
2023-09-11 22:59:56 +0300

    rentboats

--- composition/main.go
+++ composition/main.go
@@ -6,15 +6,21 @@ import (
 )
 
 func main() {
-	boats := []*store.Boat{
-		store.NewBoat("Kayak", 275, 1, false),
-		store.NewBoat("Canoe", 400, 3, false),
-		store.NewBoat("Tender", 650.25, 2, true),
-	}
+	// boats := []*store.Boat{
+	// 	store.NewBoat("Kayak", 275, 1, false),
+	// 	store.NewBoat("Canoe", 400, 3, false),
+	// 	store.NewBoat("Tender", 650.25, 2, true),
+	// }
 
 	//lifejacket := &store.Product{Name: "Lifejacket", Category: "Watersports"}
 
-	for _, b := range boats {
-		fmt.Println("Boat:", b.Name, "Price:", b.Price(0.2))
+	rentals := []*store.RentalBoat{
+		store.NewRentalBoat("Rubber Ring", 10, 1, false, false, "N/A", "N/A"),
+		store.NewRentalBoat("Yacht", 50000, 5, true, true, "Bob", "Alice"),
+		store.NewRentalBoat("Super Yacht", 100000, 15, true, true, "Dora", "Charlie"),
+	}
+
+	for _, r := range rentals {
+		fmt.Println("Boat:", r.Name, "Price:", r.Price(0.2), "Captain:", r.Captain)
 	}
 }
