f316c8cba194db9d9ba725fb7ea64bb9829f7d1b
Sergey Slobodyanyuk <sslobodyanyuk@ocs.ru>
2023-11-06 12:42:28 +0300

    select before

--- concurency/main.go
+++ concurency/main.go
@@ -5,15 +5,21 @@ import (
 	//"time"
 )
 
+func reciveDispatches(channel <-chan DispatchNotification) {
+	for details := range channel {
+		fmt.Println("Dispatch to", details.Customer, "for", details.Quantity, details.Product.Name)
+	}
+	fmt.Println("Channel closed")
+}
+
 func main() {
 	// fmt.Println("main function started")
 	// CalcStoreTotal(Products)
 	// //time.Sleep(time.Second * 5)
 	// fmt.Println("main function complete")
 	dispatchChannel := make(chan DispatchNotification, 100)
-	go DispatchOrders(dispatchChannel)
-	for details := range dispatchChannel {
-		fmt.Println("Dispatch to", details.Customer, "for", details.Quantity, details.Product.Name)
-	}
-	fmt.Println("Channel closed")
+	// var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
+	// var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
+	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
+	reciveDispatches((<-chan DispatchNotification)(dispatchChannel))
 }
