2bc9eeed8f1975ac85e8ed8daa9c1dc583e6b613
Sergey Slobodyanyuk <sslobodyanyuk@ocs.ru>
2023-11-06 12:50:15 +0300

    t

--- concurency/main.go
+++ concurency/main.go
@@ -1,16 +1,13 @@
 package main
 
-import (
-	"fmt"
-	//"time"
-)
+//"time"
 
-func reciveDispatches(channel <-chan DispatchNotification) {
-	for details := range channel {
-		fmt.Println("Dispatch to", details.Customer, "for", details.Quantity, details.Product.Name)
-	}
-	fmt.Println("Channel closed")
-}
+// func reciveDispatches(channel <-chan DispatchNotification) {
+// 	for details := range channel {
+// 		fmt.Println("Dispatch to", details.Customer, "for", details.Quantity, details.Product.Name)
+// 	}
+// 	fmt.Println("Channel closed")
+// }
 
 func main() {
 	// fmt.Println("main function started")
@@ -20,6 +17,9 @@ func main() {
 	dispatchChannel := make(chan DispatchNotification, 100)
 	// var sendOnlyChannel chan<- DispatchNotification = dispatchChannel
 	// var receiveOnlyChannel <-chan DispatchNotification = dispatchChannel
-	go DispatchOrders(chan<- DispatchNotification(dispatchChannel))
-	reciveDispatches((<-chan DispatchNotification)(dispatchChannel))
+	go DispatchOrders((chan<- DispatchNotification)(dispatchChannel))
+	//reciveDispatches((<-chan DispatchNotification)(dispatchChannel))
+	for {
+		select {}
+	}
 }
--- concurency/orderdispatch.go
+++ concurency/orderdispatch.go
@@ -16,13 +16,14 @@ var Customers = []string{"Alice", "Bob", "Charlie", "Dora"}
 
 func DispatchOrders(channel chan<- DispatchNotification) {
 	rand.Seed(time.Now().UTC().UnixNano())
-	orderCount := rand.Intn(3) + 2
+	orderCount := rand.Intn(3) + 5
 	fmt.Println("Order Count: ", orderCount)
 	for i := 0; i < orderCount; i++ {
 		customer := Customers[rand.Intn(len(Customers)-1)]
 		product := ProductList[rand.Intn(len(ProductList)-1)]
 		quantity := rand.Intn(10)
 		channel <- DispatchNotification{customer, product, quantity}
+		time.Sleep(time.Millisecond * 750)
 	}
 	close(channel)
 }
