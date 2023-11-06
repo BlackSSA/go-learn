8f2604ea7b2fb81e94952e9ea16226be2ced70b9
Sergey Slobodyanyuk <sslobodyanyuk@ocs.ru>
2023-11-06 11:29:32 +0300

    Дошел до select

--- concurency/main.go
+++ concurency/main.go
@@ -2,10 +2,18 @@ package main
 
 import (
 	"fmt"
+	//"time"
 )
 
 func main() {
-	fmt.Println("main function started")
-	CalcStoreTotal(Products)
-	fmt.Println("main function complete")
+	// fmt.Println("main function started")
+	// CalcStoreTotal(Products)
+	// //time.Sleep(time.Second * 5)
+	// fmt.Println("main function complete")
+	dispatchChannel := make(chan DispatchNotification, 100)
+	go DispatchOrders(dispatchChannel)
+	for details := range dispatchChannel {
+		fmt.Println("Dispatch to", details.Customer, "for", details.Quantity, details.Product.Name)
+	}
+	fmt.Println("Channel closed")
 }
---
+++ concurency/orderdispatch.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+type DispatchNotification struct {
+	Customer string
+	*Product
+	Quantity int
+}
+
+var Customers = []string{"Alice", "Bob", "Charlie", "Dora"}
+
+func DispatchOrders(channel chan DispatchNotification) {
+	rand.Seed(time.Now().UTC().UnixNano())
+	orderCount := rand.Intn(3) + 2
+	fmt.Println("Order Count: ", orderCount)
+	for i := 0; i < orderCount; i++ {
+		customer := Customers[rand.Intn(len(Customers)-1)]
+		product := ProductList[rand.Intn(len(ProductList)-1)]
+		quantity := rand.Intn(10)
+		channel <- DispatchNotification{customer, product, quantity}
+	}
+	close(channel)
+}
